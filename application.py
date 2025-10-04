f64b16ffc2520dd8a266f4e98a5416b46f23e264
Shubham959 <sk8545785@gmail.com>
2025-10-04 20:44:06 +0530

    Fix Flask port binding for Render deployment

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 import numpy as np
 import pandas as pd
@@ -41,5 +42,6 @@ def predict_datapoint():
 
 
 if __name__ == ("__main__"):
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
